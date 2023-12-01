6bf245843e8864097655f2ce99584d48d28829f9
Stephanie Lakner <stephanie.lakner@rub.de>
2023-12-01 17:07:39 +0100

    1.1

---
+++ 1/main.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+using std::string;
+#include <iostream>
+
+int main()
+{
+	std::ifstream input("input.txt");
+	string line;
+	const string digits = "0123456789";
+	int sum = 0;
+	while (input >> line)
+		sum += stoi(line.substr(line.find_first_of(digits), 1) + 
+					line.substr(line.find_last_of(digits), 1));
+	std::cout << sum << std::endl;
+}
