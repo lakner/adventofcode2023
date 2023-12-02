dc9f47b8fff45be50e81adb20bc973ab88c67a53
Stephanie Lakner <stephanie.lakner@gdata-adan.de>
2023-12-02 16:21:18 +0100

    2_1

--- 1/1_1.cpp
+++ 1/1_1.cpp
@@ -5,12 +5,12 @@ using std::string;
 
 int main()
 {
-	std::ifstream input("input.txt");
+	std::ifstream input("test.txt");
 	string line;
 	const string digits = "0123456789";
 	int sum = 0;
 	while (input >> line)
-		sum += stoi(line.substr(line.find_first_of(digits), 1) + 
+		sum += stoi(line.substr(line.find_first_of(digits), 1) +
 					line.substr(line.find_last_of(digits), 1));
 	std::cout << sum << std::endl;
 }
